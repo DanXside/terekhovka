10c21dd2b20982e94d5f1ec3974d2ece8e05131b
DanXside <danya.moiseev@inbox.ru>
2023-05-11 13:28:12 +1000

    promo

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.single-item').slick({
+        infinite: true,
+        arrows: false,
+        autoplay: true,
+        dots: false,
+        speed: 700
+    });
+});
