6d9d249d4ff7b5d3acc7ccb90beebbfbf988e864
DanXside <danya.moiseev@inbox.ru>
2023-05-12 13:06:33 +1000

    news

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,27 @@ $(document).ready(function(){
         dots: false,
         speed: 700
     });
+
+    $('.request__form_phone').inputmask("+7(999)999-99-99");
+    jQuery.validator.addMethod("checkMaskPhone", function (value, el) {
+        return /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/g.test(value);
+    });
+
+    const form = $('.request__form');
+
+    form.validate();
+
+    $.validator.addClassRules({
+        'request__form_phone': {
+            checkMaskPhone: true
+        }
+    });
+
+    form.submit(function(e) {
+        e.preventDefault();
+        if (form.valid()) {
+            alert('Форма отправлена');
+        };
+        return;
+    });
 });
