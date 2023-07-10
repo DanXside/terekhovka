23713ffe4d42336b6d6aebfe0368f1012aa3239e
Daniil <danya.moiseev@inbox.ru>
2023-07-10 11:25:25 +1000

    calc

--- dist/js/calc.js
+++ dist/js/calc.js
@@ -7,6 +7,14 @@ $(document).ready(function () {
         $('.calc').fadeOut(300);
     });
 
+    $('#calc-brick').change(function() {
+        let index = $('#calc-brick')[0].selectedIndex;
+        photo = $('#calc-brick option').eq(index).data('photo');
+        console.log(photo);
+        $('#calc-brick-photo').css('backgroundImage', 'url(../'+ photo +')');
+    });
+    $('#calc-brick').change();
+
     $('#calc-sb').click(function (e) {
         e.preventDefault();
         function getVal (id) {
@@ -25,7 +33,7 @@ $(document).ready(function () {
           brick = $('#calc-brick').val().split('/'),
           P1 = parseFloat(brick[0] / 1000),
           P2 = parseFloat(brick[1] / 1000),
-          P3 = parseFloat(brick[3] / 1000),
+          P3 = parseFloat(brick[2] / 1000),
           brickVolume = (P1 + semiWidth) * (P2 + semiWidth) * P3,
           quantityBricks = Math.round(((perimeter * height - doorsWidth * doorsQuantity * doorsHeight - windowQuantity * windowWidth * windowHeight) * brickworkWidth ) / brickVolume );
 
@@ -34,15 +42,9 @@ $(document).ready(function () {
             $('#calc-output').html('<p>Ошибка вычисления</p>');
         } else {
             $('#calc-output').fadeIn();
-            $('#calc-output').html('<p>'+ quantityBricks +' шт</p>')
+            $('#calc-output').css('display', 'flex');
+            $('#calc-output').html('<span>Количество материала: </span><p>'+ quantityBricks +' шт</p>')
         };
-
-        $('#calc-brick').change(function() {
-            let index = $('#calc-brick')[0].selectedIndex;
-            photo = $('#calc-brick option').eq(index).data('photo');
-            $('#calc-brick-photo').css('backgroundImage', 'url('+ photo +')');
-        });
-        $('#calc-brick').change();
     });
 
     
